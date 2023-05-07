ede67c54a95ad84c25fc7ddfbe060cf5a7066010
samuel-barkley <60850711+samuel-barkley@users.noreply.github.com>
2023-05-07 17:36:01 +0200

    Update Assets/Scripts/GameManager/GameManager.cs

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public partial class GameManager : MonoBehaviour
 {
-    int interval = 1;
-    float nextTime = 0;
+    private int interval = 1;
+    private float nextTime = 0;
 
     // Start is called before the first frame update
     void Start()
